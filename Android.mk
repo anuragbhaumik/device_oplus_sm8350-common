BittuBomda/android_device_oplus_sm8350-common

sm8350-common: Convert tree to Oplus

Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter lemonade lemonadep,$(TARGET_DEVICE)),)
+ifneq ($(filter martini OP524F RMX2202 RMX3310 RMX3360 RMX3461,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)

anuragbhaumik/device_oplus_sm8350-common

Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter lemonade lemonadep,$(TARGET_DEVICE)),)
+ifneq ($(filter cupid martini OP524F RMX3312 RMX3360 RMX3461,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)